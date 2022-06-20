494cbd3142e59feb7604e4df246abc8f1e572c82
Katie Eells <katieeells@Katies-MBP.lan>
2022-06-20 14:52:46 -0400

    added some suspense and a spinner

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import "./App.css";
+import React, { useEffect, useState, Suspense } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { createResource as fetchData } from './helper'
+import Spinner from './components/Spinner'
 
 function App(){
   // state components
-    let [search, setSearch] = useState('')
+    let [searchTerm, setSearchTerm] = useState("")
     let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
-
-    const API_URL = 'https://itunes.apple.com/search?term='
+    let [data, setData] = useState(null)
 
     // the hook
     useEffect(() => {
-      // conditional added to prevent the state from running if there is no search term
-      if(search) {
-        const fetchData = async () => {
-            document.title = `${search} Music`
-            const response = await fetch(API_URL + search)
-            const resData = await response.json()
-            if (resData.results.length > 0) {
-                setData(resData.results)
-            } else {
-                setMessage('Not Found')
-            }
+        if (searchTerm) {
+            setData(fetchData(searchTerm));
         }
-        fetchData()
-      }
-  }, [search])
-  
+        }, [searchTerm]);
+
   // function to pass down to SearchBar with 2 arguements (event object and search term typed into searchbar)
-  const handleSearch = (e, term) => {
-    e.preventDefault()
-    setSearch(term)
-  }
+    const handleSearch = (e, term) => {
+      e.preventDefault()
+      setSearchTerm(term)
+    }
 
-  return (
-      <div>
-          <SearchBar handleSearch = {handleSearch} />
+    const renderGallery = () => {
+        if(data){
+          return (
+            <Suspense fallback={<Spinner />}>
+              <Gallery data={data} />
+            </Suspense>
+          )
+        }
+      }
+    
+      return (
+        <div className="App">
+          <SearchBar handleSearch={handleSearch} />
           {message}
-          {/* data being sent to Gallery component to be viewed */}
-          <Gallery data={data} />
-      </div>
-  )
-
-}
-
-export default App
+          {renderGallery()}
+        </div>
+      );
+    }
+    
+    export default App;
